7d907d84c69185f5db5a3fb427de56979bc60ca3
Avinash Gosavi <aag1091@gmail.com>
2015-09-08 19:37:08 -0400

    Added change for banner bg and moved new acm site to root

---
+++ contact_us.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by TEMPLATED
+http://templated.co
+Released for free under the Creative Commons Attribution License
+
+Name       : BarbedFlower
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20140322
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Varela" rel="stylesheet" />
+<link href="bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+</head>
+<body>
+<div id="wrapper">
+  <div id="header-wrapper">
+    <div id="header" class="container">
+      <div id="logo">
+        <img src="images/acm_logo.png">
+        &nbsp;&nbsp;&nbsp;
+        <a href="#">ODU ACM</a>
+      </div>
+      <div id="menu">
+        <ul>
+          <li><a href="index.html" accesskey="1" title="">Home</a></li>
+          <li><a href="about_us.html" accesskey="2" title="">About Us</a></li>
+          <li><a href="#" accesskey="2" title="">Events</a></li>
+          <li class="active"><a href="contact_us.php" accesskey="5" title="">Contact Us</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div id="banner">
+    <div class="container">
+      <div class="title">
+        <h2>Association for Computing Machinery</h2>
+        <span class="byline">at Old Dominion University</span> </div>
+    <!--  <ul class="actions">
+        <li><a href="#" class="button">Etiam posuere</a></li>
+      </ul> -->
+    </div>
+  </div>
+
+<div class="container">
+  <!-- <iframe src="https://docs.google.com/forms/d/1ehaX6eEV4UM7xBmxT1ip48fe4dAQXR1TlgOWjd9nKqY/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe> -->
+</div>
+
+</div>
+
+<div id="copyright" class="container">
+  <p>Old Dominion University ACM © 2015 </p>
+</div>
+</body>
+</html>
